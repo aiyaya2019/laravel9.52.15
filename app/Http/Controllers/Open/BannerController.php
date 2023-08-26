32dfab0d50a060a48a2ae2e8bd9dd479cff0bbb4
wf <wf@qq.com>
2023-08-26 10:12:31 +0800

    feat:后台不需校验权限的接口和路由

---
+++ app/Http/Controllers/Open/BannerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Open;
+
+use App\Http\BusinessLogic\Banner\BannerLogic;
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+
+class BannerController extends BaseController {
+    protected $logic;
+
+    public function __construct() {
+        parent::__construct();
+        $this->logic = new BannerLogic();
+    }
+
+    public function list() {
+        $data = $this->logic->list();
+    }
+
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -19,22 +19,18 @@ Route::get('/', function () {
 
 Route::match(['get', 'post'], 'server/login', [\App\Http\Controllers\Admin\LoginController::class, 'login']);
 
+// 后台需要校验权限的接口
 Route::prefix('/server')->middleware('auth.admin')->group(function () {
     Route::match(['get', 'post'], 'logout', [\App\Http\Controllers\Admin\LoginController::class, 'logout']);
 
     Route::prefix('/banner')->group(function () {
         Route::match(['get', 'post'], 'list', [\App\Http\Controllers\Admin\BannerController::class, 'list']);
-        Route::match(['get', 'post'], 'list', [\App\Http\Controllers\Admin\BannerController::class, 'list']);
     });
 });
 
-
-// Route::prefix('/server')->group(function () {
-//     Route::prefix('/banner')->group(function () {
-//         Route::match(['get', 'post'], 'list', [\App\Http\Controllers\Admin\BannerController::class, 'list']);
-//         Route::match(['get', 'post'], 'list', [\App\Http\Controllers\Admin\BannerController::class, 'list']);
-//     });
-//     Route::prefix('/login')->group(function () {
-//         Route::match(['get', 'post'], 'login', [\App\Http\Controllers\Admin\LoginController::class, 'login']);
-//     });
-// });
+// 不需校验权限的接口
+Route::prefix('/open')->group(function () {
+    Route::prefix('/banner')->group(function () {
+        Route::match(['get', 'post'], 'list', [\App\Http\Controllers\Open\BannerController::class, 'list']);
+    });
+});
