2f4655554113f46393bffcbca0766e0bfc7eb313
wf <wf@qq.com>
2023-08-26 10:41:39 +0800

    feat:后台接口加参数debug=test跳过登录验证

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -16,6 +16,11 @@ class AdminAuth {
      */
     public function handle(Request $request, Closure $next) {
 
+        // 跳过登录验证
+        if (!empty($request->input('debug')) && $request->input('debug') == 'test') {
+            return $next($request);
+        }
+
         if (!Auth::guard('admin')->check()) {
             return response()->json(['code' => 300, 'msg' => '没有登录']);
         }
