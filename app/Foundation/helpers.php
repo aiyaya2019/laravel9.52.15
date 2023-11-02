45ef23b27914452ec893a976f90e0ad1d8c5c693
wf <wf@qq.com>
2023-11-02 16:09:30 +0800

    feat:完善日志

--- app/Foundation/helpers.php
+++ app/Foundation/helpers.php
@@ -41,15 +41,16 @@ function returnData($code = 200, $msg = '操作成功', $total = 0, $data = []) 
  * @Time: 2023/11/1 21:41
  */
 function requestLog(int $reqStatus, string $useTime = '', array $returnData = [], string $model = 'adminlog') {
-
     $data = [
         'method' => request()->method(),//请求方式
         'port' => request()->getPort(),//端口
         'ip' => request()->ip(),//ip
         'url' => request()->url(),//url
+        'return_code' => $returnData['code'],//返回状态码：200成功，201成功弹出确认窗口，300去登录，400失败
+        'return_msg' => $returnData['msg'],//返回信息提示
+        'return_data' => env('LOG_RECORD_RETURN_DATA') == 1 ? $returnData : '不记录返回数据',//返回数据
         'params' => request()->all(),//请求参数
-        'return_data' => $returnData,//返回数据
-        'header' => request()->header(),
+        'header' => env('LOG_HEADER_FORMAT') == 1 ? request()->header() : json_encode(request()->header()),
     ];
 
     if (env('LOG_FORMAT') == 1) {
