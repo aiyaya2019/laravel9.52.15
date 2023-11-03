e490badab46d10de206fdd43c3e47ab763264ff8
wf <wf@qq.com>
2023-11-03 16:08:21 +0800

    feat:注销Schema::defaultStringLength(191);

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider {
      * @return void
      */
     public function boot() {
-        Schema::defaultStringLength(191);
+        // Schema::defaultStringLength(191);//解决迁移报错PDOException::("SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 1000 bytes")
 
         DB::listen(
             function ($query) {
