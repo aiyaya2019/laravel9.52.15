8b2e83decca9c4796ed77df4ca8720ecc0d477cb
wf <wf@qq.com>
2023-11-16 19:44:19 +0800

    feat:多工作簿导出demo

--- app/Http/Controllers/Demo/ExportController.php
+++ app/Http/Controllers/Demo/ExportController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Demo;
 
 use App\Http\Controllers\Controller;
+use App\Http\Excel\MoreSheetExport;
 use App\Http\Excel\SampleExport;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ExportController extends Controller {
 
+    /**
+     * @Desc:简单导出
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     * @author: wanf
+     * @Time: 2023/11/16 19:24
+     */
     public function export() {
         $data = [
             [
@@ -27,6 +34,126 @@ class ExportController extends Controller {
         ];
 
         return Excel::download(new SampleExport($data, '用户列表'), 'sample_export.xlsx');
+    }
+
+    /**
+     * @Desc:多工作簿导出
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     * @author: wanf
+     * @Time: 2023/11/16 19:24
+     */
+    public function moreSheetExport() {
+        // 数据格式
+        $data5 = [
+            [
+                'row_num' => 2,
+                'data' =>
+                    [
+                        [
+                            'type' => '关注名单',
+                            'name' => '人员1',
+                            'total' => '10',
+                            'days' => 5,
+                            'num' => 8,
+                        ],
+                        [
+                            'type' => '关注名单',
+                            'name' => '人员2',
+                            'total' => '20',
+                            'days' => 5,
+                            'num' => 8,
+                        ],
+                    ],
+            ],
+            [
+                'row_num' => 1,
+                'data' =>
+                    [
+                        [
+                            'type' => '介入名单',
+                            'name' => '人员3',
+                            'total' => '10',
+                            'days' => 5,
+                            'num' => 8,
+                        ]
+                    ],
+            ],
+            [
+                'row_num' => 2,
+                'data' =>
+                    [
+                        [
+                            'type' => '公关名单',
+                            'name' => '人员4',
+                            'total' => '10',
+                            'days' => 5,
+                            'num' => 8,
+                        ],
+                        [
+                            'type' => '公关名单',
+                            'name' => '人员5',
+                            'total' => '20',
+                            'days' => 5,
+                            'num' => 8,
+                        ],
+                    ],
+            ],
+        ];
+
+        $data7 = [
+
+            [
+                'row_num' => 2,
+                'data' =>
+                    [
+                        [
+                            'type' => '人员1',
+                            'name' => '10',
+                            'total' => '100',
+                            'days' => '',
+                            'num' => '',
+                        ],
+                        [
+                            'type' => '人员2',
+                            'name' => '20',
+                            'total' => '200',
+                            'days' => '',
+                            'num' => '',
+                        ],
+                    ],
+            ],
+            [
+                'row_num' => 1,
+                'data' =>
+                    [
+                        [
+                            'type' => '人员3',
+                            'name' => '30',
+                            'total' => '300',
+                            'days' => '',
+                            'num' => '',
+                        ]
+                    ],
+            ],
+        ];
+
+        $data = [
+            'cusromer_warning' => [
+                'sheet_name' => '客户预警',
+                'data' => $data5,
+            ],
+            'product_warning' => [
+                'sheet_name' => '产品预警',
+                'data' => $data5,
+            ],
+            'customer_ranking' => [
+                'sheet_name' => '客户排行榜',
+                'data' => $data7,
+            ],
+        ];
+
+        return Excel::download(new MoreSheetExport($data, '', $data['cusromer_warning']['data']), '多工作簿导出.xls');
+
     }
 
 }
---
+++ app/Http/Excel/MoreSheetExport.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Excel;
+
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithTitle; //sheet名称
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;//sheet
+use Maatwebsite\Excel\Concerns\WithStyles;//造型
+use Maatwebsite\Excel\Concerns\WithColumnWidths; //列宽
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+
+class MoreSheetExport implements WithHeadings, FromCollection, WithTitle, WithColumnWidths, WithStyles, WithColumnFormatting, WithMultipleSheets{
+    use Exportable;
+
+    private $data;//数据
+    private $sheetName = ''; //sheet名称
+    private $column; //总行数
+    private $sheetData = [];
+    private $header;
+
+    public function __construct($data, $sheetName = '', $sheetData = [], $header = 1) {
+        $this->data = $data;
+        $this->sheetName = $sheetName;
+        $this->sheetData = $sheetData;
+        $this->header = $header;
+    }
+
+    /**
+     * 表头
+     * @return string[]
+     */
+    public function headings(): array {
+        if ($this->header == 1) {
+            $header = ['预警类别', '客户名称', '售后总数', '售后涉及天数', '售后涉及产品数'];
+        } else {
+            $header = ['客户名称', '客户问题售后数', '产生费用'];
+        }
+        return $header;
+    }
+
+    /**
+     * 导出
+     * @return \Illuminate\Support\Collection|\Tightenco\Collect\Support\Collection
+     */
+    public function collection() {
+        $sheetData = $this->sheetData;
+        $list = [];
+        foreach ($sheetData as $key => $value) {
+            foreach ($value['data'] as $val) {
+                $list[] = [
+                    'type' => $val['type'],
+                    'name' => $val['name'],
+                    'total' => $val['total'],
+                    'days' => $val['days'],
+                    'num' => $val['num'],
+                ];
+            }
+        }
+        $this->column = count($list);
+
+        return collect($list);
+    }
+
+    /**
+     * 格式化列
+     * @return array
+     */
+    public function columnFormats(): array {
+        return [];
+    }
+
+    /**
+     * 设置列宽
+     * @return array
+     */
+    public function columnWidths(): array {
+        return [
+            'A' => 40, 'B' => 40, 'C' => 40, 'D' => 40, 'E' => 40
+        ];
+    }
+
+    /**
+     * 样式设置
+     * @param Worksheet $sheet
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    public function styles(Worksheet $sheet) {
+        $sheet->getRowDimension('1')->setRowHeight(24);
+
+        $sheet->getStyle('A1:G1')->getFont()->setBold(true)->getColor();
+        $sheet->getStyle('A1:G1'.$this->column)->getAlignment()->setVertical('center');//垂直居中
+        $sheet->getStyle('A1:G1'.$this->column)->applyFromArray(['alignment' => ['horizontal' => 'center']]);//设置水平居中
+
+        $row = 2;
+
+        foreach ($this->sheetData as $value) {
+            $endRow = $row + $value['row_num'] - 1;
+            $sheet->mergeCells("A$row:A$endRow");
+            $row = $endRow + 1;
+        }
+    }
+
+    /**
+     * sheet名称
+     * @return string
+     */
+    public function title(): string {
+        return $this->sheetName;
+    }
+
+    /**
+     * 多工作簿
+     * @return string
+     */
+    public function sheets(): array {
+        $sheets = [];
+
+        foreach ($this->data as $key => $value) {
+            unset($this->data[$key]);
+            $header = $key == 'customer_ranking' ? 2 : 1;
+            $sheets[] = new MoreSheetExport($this->data, $value['sheet_name'], $value['data'], $header);
+        }
+
+        return $sheets;
+    }
+}
